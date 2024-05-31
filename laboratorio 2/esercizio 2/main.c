24660caabd71caddaac363e7d4c0391ae8f017d9
SafiaAbdirashid <114006045+SafiaAbdirashid@users.noreply.github.com>
2024-05-31 10:06:38 +0200

    Update main.c

--- laboratorio 2/esercizio 2/main.c
+++ laboratorio 2/esercizio 2/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int N, n0 = 0, n1 = 1, c;
+    printf("inserisci N:");
+    scanf("%d", &N);
+    printf("i primi %d numeri della serie di fibonacci sono:\n", N);
+    for (int i =0; i<N; i++) {
+        printf("%d", n0);
+        c = n1;
+        n1 = n0 + n1;
+        n0 = c;
+    }
+    return 0;
+}
