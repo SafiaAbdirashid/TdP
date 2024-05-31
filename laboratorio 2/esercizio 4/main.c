80a39da37353bec6742c1d34e6899cc6b80f936c
SafiaAbdirashid <114006045+SafiaAbdirashid@users.noreply.github.com>
2024-05-31 10:12:06 +0200

    Update main.c

--- laboratorio 2/esercizio 4/main.c
+++ laboratorio 2/esercizio 4/main.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
 
+int main() {
+    FILE *fin, *fout;
+    char c0, c1;
+    int h=0;
+    fin= fopen("../sorgente.txt", "r");
+    fout= fopen("..//ricodificato.txt", "w");
+    c0=fgetc(fin);
+    while (!feof(fin)) {
+        c1=fgetc(fin);
+        if (isalpha(c0)) {
+            if (isupper(c0)) {
+                if (isalpha(c1)) {
+                    h=c0-'A';
+                    c1=c1+h;
+                    if (c1>122) {
+                        c1=c1-26;
+                    }
+                }
+            }
+            else if (islower(c0)){
+                if (isalpha(c1)) {
+                    h=c0-'a';
+                    c1=c1+h;
+                    if (c1>122) {
+                        c1=c1-26;
+                    }
+                }
+            }
+            else {
+                printf(fout, "%c", c0);
+            }
+            fprintf(fout, "%c", c0);
+            c0=c1;
+        }
+        else {
+            fprintf(fout, "%c", c0);
+            c0=c1;
+        }
+    }
+    return 0;
+}
