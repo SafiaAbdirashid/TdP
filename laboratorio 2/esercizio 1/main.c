c3b6ebe33440b2a86336612c07be372ba0f48f81
SafiaAbdirashid <114006045+SafiaAbdirashid@users.noreply.github.com>
2024-05-31 10:05:03 +0200

    Update main.c

--- laboratorio 2/esercizio 1/main.c
+++ laboratorio 2/esercizio 1/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+/*acquisisco due numeri interi e calcolo utilizzando la formula di eurelo il loro mcd*/
+int main() {
+    int a, b, i, mcd;
+    printf("inserisci numero positivo a:");
+    scanf("%d", &a);
 
+    printf("inserisci numero positivo b:");
+    scanf("%d", &b);
+    for (int i = 1; i <= a ; ++i) {
+        if (a % i == 0) {
+            if (b % i == 0) {
+                mcd = i;
+            }
+        }
+    }
+    printf("il mcd di %d e %d e': %d \n", a, b, mcd);
+    return 0;
+}
