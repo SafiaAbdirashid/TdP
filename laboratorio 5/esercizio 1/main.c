7a6da539b6a0274da2c11cd5e5edaaf141e6f429
SafiaAbdirashid <114006045+SafiaAbdirashid@users.noreply.github.com>
2024-06-01 14:55:43 +0200

    Update main.c

--- laboratorio 5/esercizio 1/main.c
+++ laboratorio 5/esercizio 1/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#define N 20
+#define M 20
+int main() {
+    int mat[N][M];
+    int n, m;
+    FILE *fin, *fout;
+    fin= fopen("../mat.txt", "r");
+    fout= fopen("../output.txt", "w");
 
+    while (!feof(fin)) {
+        fscanf(fin, "%d %d", &n, &m);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                fscanf(fin, "%d", &mat[i][j]);
+            }
+        }
+    }
+    for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            if (mat[i][j]==3) {
+                fprintf(fout, " il vincitore della giornata %d e': %d\n", j+1, i+1);
+            }
+        }
+    }
+    return 0;
+}
